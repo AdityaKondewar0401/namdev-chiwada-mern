aef5fe7a07b6f866d000623e57751a500fa96008
Aditya Prashant Kondewar <156500995+AdityaKondewar0401@users.noreply.github.com>
2026-04-21 18:24:12 +0530

    cartpage2

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { getCart, addToCart, updateCartItem, removeFromCart, clearCart } = require('../controllers/cartController');
+
+const {
+  getCart,
+  addToCart,
+  updateCartItem,
+  removeFromCart,
+  clearCart,
+} = require('../controllers/cartController');
+
 const { protect } = require('../middleware/auth');
 
 router.use(protect);
+
+// Get cart
 router.get('/', getCart);
+
+// Add item
 router.post('/', addToCart);
-router.put('/:itemId', updateCartItem);
+
+// FIXED update route
+router.put('/', updateCartItem);
+
+// Remove single item
 router.delete('/:itemId', removeFromCart);
+
+// Clear all cart
 router.delete('/', clearCart);
 
 module.exports = router;
